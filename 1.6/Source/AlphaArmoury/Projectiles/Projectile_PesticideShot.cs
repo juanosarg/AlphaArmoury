3d7477dd7749c13f7b424b68ebd20e5749c45aff
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-09-13 17:29:17 +0200

    v0.1

---
+++ 1.6/Source/AlphaArmoury/Projectiles/Projectile_PesticideShot.cs
@@ -0,0 +1,57 @@
+﻿using Verse;
+using RimWorld;
+using System;
+using UnityEngine;
+using UnityEngine.Analytics;
+using Verse.Noise;
+using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
+using System.Collections.Generic;
+
+namespace AlphaArmoury
+{
+    public class Projectile_PesticideShot : Projectile_Explosive
+    {
+
+        protected override void Explode()
+        {
+
+
+            IntVec3 position = Position;
+            float explosionRadius = def.projectile.explosionRadius;
+
+            int num = GenRadial.NumCellsInRadius(explosionRadius);
+            List<Thing> blighToDelete = new List<Thing>();
+            for (int i = 0; i < num; i++)
+            {
+                IntVec3 intVec = position + GenRadial.RadialPattern[i];
+                if (!intVec.InBounds(Map))
+                {
+                    continue;
+                }
+                foreach (Thing victim in intVec.GetThingList(Map))
+                {
+                    Blight blight = victim as Blight;
+
+                    if (blight != null && !blight.Destroyed)
+                    {
+                        blighToDelete.Add(blight);
+                        
+
+                    }
+
+                }
+                if(blighToDelete.Count > 0)
+                {
+                    foreach(Thing blight in blighToDelete)
+                    {
+                        blight.Destroy();
+                    }
+                }
+
+                
+
+            }
+            base.Explode();
+        }
+    }
+}
---
+++ 1.6/Source/AlphaArmoury/Projectiles/Projectile_SpawnsSteelThing.cs
@@ -0,0 +1,31 @@
+﻿
+using RimWorld;
+using Verse;
+namespace AlphaArmoury
+{
+    public class Projectile_SpawnsSteelThing : Projectile
+    {
+        protected override void Impact(Thing hitThing, bool blockedByShield = false)
+        {
+            Map map = base.Map;
+            base.Impact(hitThing, blockedByShield);
+            IntVec3 loc = base.Position;
+            if (def.projectile.tryAdjacentFreeSpaces && base.Position.GetFirstBuilding(map) != null)
+            {
+                foreach (IntVec3 item in GenAdjFast.AdjacentCells8Way(base.Position))
+                {
+                    if (item.GetFirstBuilding(map) == null && item.Standable(map))
+                    {
+                        loc = item;
+                        break;
+                    }
+                }
+            }
+            Thing thing = GenSpawn.Spawn(ThingMaker.MakeThing(def.projectile.spawnsThingDef,ThingDefOf.Steel), loc, map);
+            if (thing.def.CanHaveFaction)
+            {
+                thing.SetFaction(base.Launcher.Faction);
+            }
+        }
+    }
+}
