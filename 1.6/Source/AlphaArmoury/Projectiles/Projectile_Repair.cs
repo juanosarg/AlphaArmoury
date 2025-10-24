67a4b98b46188874ce6adff449f709ce81a10bc9
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-10-24 19:39:14 +0200

    v0.1

---
+++ 1.6/Source/AlphaArmoury/Projectiles/Projectile_Repair.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AlphaArmoury
+{
+    public class Projectile_Repair : Projectile_Sinusoid
+    {
+
+        protected override void Impact(Thing hitThing, bool blockedByShield = false)
+        {
+            Map map = base.Map;
+           // base.Impact(hitThing, blockedByShield);
+            Building building = hitThing as Building;
+            if (building != null)
+            {
+                Thing feedbackProjectile = ThingMaker.MakeThing(InternalDefOf.AArmoury_LifestealFeedback);
+
+                Thing feedbackProjectileLaunched = GenSpawn.Spawn(feedbackProjectile, ExactPosition.ToIntVec3().RandomAdjacentCell8Way(), map);
+                if (feedbackProjectileLaunched is Projectile piece) piece.Launch(pawn, launcher, launcher, ProjectileHitFlags.All);
+
+
+            }
+        }
+    }
+}
