94bb1b469f009fc1ad706de69e5e84f3b7983379
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-10-25 17:28:27 +0200

    v0.1

---
+++ 1.6/Source/AlphaArmoury/DamageWorkers/DamageWorker_Corrosive.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AlphaArmoury
+{
+    public class DamageWorker_Corrosive: DamageWorker_AddInjury
+    {
+
+        public override DamageResult Apply(DamageInfo dinfo, Thing thing)
+        {
+            
+            Pawn pawn = thing as Pawn;
+            if (pawn?.apparel != null)
+            {
+                foreach(Apparel apparel in pawn.apparel.WornApparel)
+                {
+                    DamageInfo info2 = dinfo;
+                    info2.SetAmount(dinfo.Amount*3);
+                    apparel.TakeDamage(info2);
+
+                }
+            }
+            return base.Apply(dinfo, thing);
+        }
+    }
+}
