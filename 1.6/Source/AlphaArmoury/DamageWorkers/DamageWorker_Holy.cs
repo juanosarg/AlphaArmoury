af742ffd1f8f5e37876a09ce3996287a86807f3f
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-10-26 09:34:43 +0100

    v0.1

---
+++ 1.6/Source/AlphaArmoury/DamageWorkers/DamageWorker_Holy.cs
@@ -0,0 +1,26 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AlphaArmoury
+{
+    public class DamageWorker_Holy : DamageWorker_AddInjury
+    {
+
+        public override DamageResult Apply(DamageInfo dinfo, Thing thing)
+        {
+
+            Pawn pawn = thing as Pawn;
+            if (pawn != null)
+            {
+                if (pawn.IsShambler) {
+                    return base.Apply(dinfo, thing);
+                }
+            }
+            return new DamageWorker.DamageResult(); 
+        }
+    }
+}
