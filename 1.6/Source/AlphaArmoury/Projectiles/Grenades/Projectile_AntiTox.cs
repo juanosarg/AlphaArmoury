98c76914df235a4f4a07c71176fc919858efffcf
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-11-05 16:38:48 +0100

    v0.1

---
+++ 1.6/Source/AlphaArmoury/Projectiles/Grenades/Projectile_AntiTox.cs
@@ -0,0 +1,46 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VanillaPlantsExpanded;
+using Verse;
+
+namespace AlphaArmoury
+{
+    public class Projectile_AntiTox : Projectile_Explosive
+    {
+
+        protected override void Explode()
+        {
+
+            IntVec3 position = Position;
+            float explosionRadius = def.projectile.explosionRadius;
+
+            int num = GenRadial.NumCellsInRadius(explosionRadius);
+            for (int i = 0; i < num; i++)
+            {
+                IntVec3 intVec = position + GenRadial.RadialPattern[i];
+                if (!intVec.InBounds(Map))
+                {
+                    continue;
+                }
+                foreach (Thing victim in intVec.GetThingList(Map))
+                {
+                    Pawn pawn = victim as Pawn;
+                    if(pawn != null)
+                    {
+                        HealthUtility.AdjustSeverity(pawn, HediffDefOf.ToxicBuildup, -0.2f);
+                        FleckMaker.ThrowMetaIcon(pawn.Position, pawn.Map, FleckDefOf.HealingCross);
+                    }
+                    
+                }
+
+            }
+            base.Explode();
+        }
+
+
+    }
+}
---
+++ 1.6/Source/AlphaArmoury/Projectiles/Projectile_Grapple.cs
@@ -0,0 +1,67 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VEF.Abilities;
+using VEF.Weapons;
+using Verse;
+using static HarmonyLib.Code;
+
+namespace AlphaArmoury
+{
+    public class Projectile_Grapple : LaserBeam
+    {
+
+        protected override void Impact(Thing hitThing, bool blockedByShield = false)
+        {
+            Map map = base.Map;
+           
+            Pawn launcherAsPawn = launcher as Pawn;
+            Pawn pawn = hitThing as Pawn;
+            IntVec3 position = Position;
+
+
+            if (pawn != null)
+            {
+               
+
+                DoJump(pawn, launcherAsPawn);
+
+            }
+            else
+            {
+             
+                
+                DoJump(launcherAsPawn, position);
+
+            }
+            base.Impact(hitThing, blockedByShield);
+        }
+
+        public static void DoJump(Pawn pawn, LocalTargetInfo currentTarget,LocalTargetInfo target = default(LocalTargetInfo), ThingDef pawnFlyerOverride = null)
+        {
+            
+           
+            IntVec3 position = pawn.Position;
+            IntVec3 cell = currentTarget.Cell;
+            Map map = pawn.Map;
+            bool flag = Find.Selector.IsSelected(pawn);
+            PawnFlyer pawnFlyer = PawnFlyer.MakeFlyer(pawnFlyerOverride ?? ThingDefOf.PawnFlyer, pawn, cell,null, null, true, null, null, target);
+            if (pawnFlyer != null)
+            {
+                FleckMaker.ThrowDustPuff(position.ToVector3Shifted() + Gen.RandomHorizontalVector(0.5f), map, 2f);
+                GenSpawn.Spawn(pawnFlyer, cell, map);
+                if (flag)
+                {
+                    Find.Selector.Select(pawn, playSound: false, forceDesignatorDeselect: false);
+                }
+                
+            }
+           
+        }
+
+
+    }
+}
